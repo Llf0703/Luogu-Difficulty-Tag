8a8cc57dc195f865bed687502a06beee49313d5b
Llf0703 <1667334662@qq.com>
2019-10-08 07:26:34 +0800

    fix a bug

--- ldt/local.py
+++ ldt/local.py
@@ -3,7 +3,7 @@ from ldt.luogu import getData
 
 def writeCsv(Type, file):
     problemIDList, problemDifficultyList = getData(Type)
-    file.write('id, dif\n')
+    file.write('id,dif\n')
     num = len(problemIDList)
     for i in range(0, num):
         file.write("".join([problemIDList[i], ', ', problemDifficultyList[i], '\n']))
